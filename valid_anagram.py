e93ff98d8dbd7b52c391c3f61b735aa255638765
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-14 15:11:54 +0530

    Add valid_anagram

---
+++ valid_anagram.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+tests = [
+    (
+        ("anagram", "nagaram",),
+        True,
+    ),
+    (
+        ("rat", "car",),
+        False,
+    ),
+]
