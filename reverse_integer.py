bd4546ac409911870059573530a1ce26573b4401
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-14 16:15:54 +0530

    Improve reverse_integer

--- reverse_integer.py
+++ reverse_integer.py
@@ -8,9 +8,15 @@ class Solution:
         reversed_num = 0
         for digit_str in reversed_str:
             digit = int(digit_str)
-            if (reversed_num > int_max // 10 or
-                    reversed_num == int_max // 10 and digit > 7):
+
+            if reversed_num > int_max // 10:
                 return 0
+
+            if reversed_num == int_max // 10:
+                if x < 0 and digit > 8:
+                    return 0
+                elif x > 0 and digit > 7:
+                    return 0
 
             reversed_num *= 10
             reversed_num += digit
