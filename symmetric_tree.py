865629a3b5c32993ee0dbe409a0297812a018daa
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-02-26 23:47:23 +0530

    Add symmetric_tree.py

---
+++ symmetric_tree.py
@@ -0,0 +1,74 @@
+from typing import Callable, Optional
+
+
+class TreeNode:
+    def __init__(self, val: int):
+        self.val = val
+        self.left: Optional[TreeNode] = None
+        self.right: Optional[TreeNode] = None
+
+
+def build_tree(
+        values: list[Optional[int]],
+        index: int = 0
+) -> Optional[TreeNode]:
+    """Builds tree out of given values"""
+    length = len(values)
+    if index >= length:
+        return None
+
+    value = values[index]
+    if value is None:
+        return None
+
+    node = TreeNode(value)
+    node.left = build_tree(values, 2*index + 1)
+    node.right = build_tree(values, 2*index + 2)
+
+    return node
+
+
+def is_mirror(
+        tree1: Optional[TreeNode],
+        tree2: Optional[TreeNode]
+) -> bool:
+    """Returns if tree1 is a mirror of tree2"""
+    if tree1 is None or tree2 is None:
+        return tree1 == tree2
+
+    return (
+        tree1.val == tree2.val
+        and is_mirror(tree1.left, tree2.right)
+        and is_mirror(tree2.left, tree1.right)
+    )
+
+
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if root is None:
+            return True
+
+        return is_mirror(root.left, root.right)
+
+
+tests = [
+    (
+        ([1, 2, 2, 3, 4, 4, 3],),
+        True,
+    ),
+    (
+        ([1, 2, 2, None, 3, None, 3],),
+        False,
+    ),
+]
+
+
+def validator(
+        isSymmetric: Callable[[Optional[TreeNode]], bool],
+        inputs: tuple[list[Optional[int]]],
+        expected: bool
+) -> None:
+    values, = inputs
+    tree = build_tree(values)
+    output = isSymmetric(tree)
+    assert output == expected, (output, expected)
