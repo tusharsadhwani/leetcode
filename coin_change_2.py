47ab81415ec162ff9d5f61f0567972ea6a7f022d
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-06-16 23:38:57 +0530

    Add comments

--- coin_change_2.py
+++ coin_change_2.py
@@ -26,14 +26,18 @@ import sys
 
 
 class Solution:
-    def change(self, amount: int, coins: list[int], index: int = 0) -> int:
+    def change(self, amount: int, coins: list[int]) -> int:
         combinations = [0 for _ in range(amount + 1)]
-        combinations[0] = 1
+        combinations[0] = 1  # 1 way to have 0 amount
 
         for coin in coins:
-            for amt in range(coin, amount + 1):
-                if amt >= coin:
-                    combinations[amt] += combinations[amt - coin]
+            for amt in range(coin, amount+1):
+                # TODO: add explanation to this. You would ususally use
+                # a 2D array for this, this is apparently a very clever
+                # optimization. Also note that if you use the
+                # "for coin in coins" loop as the inner loop, it breaks.
+                # See: https://leetcode.com/problems/coin-change-2/discuss/176706
+                combinations[amt] += combinations[amt - coin]
 
         return combinations[amount]
 
