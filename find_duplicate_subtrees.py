d821182c113b6c66778873df521d16b3fef2eea0
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-08 20:55:17 +0530

    Add find_duplicate_subtrees

---
+++ find_duplicate_subtrees.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from typing import Any, DefaultDict, Optional
+
+
+class TreeNode:
+    def __init__(
+            self, val: int,
+            left: Optional['TreeNode'] = None,
+            right: Optional['TreeNode'] = None,
+    ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findDuplicateSubtrees(self, root: TreeNode) -> list[TreeNode]:
+        trees: DefaultDict[tuple[Any, ...], list[TreeNode]] = defaultdict(list)
+
+        def preorder(root: Optional[TreeNode]) -> Optional[tuple[Any, ...]]:
+            if root is None:
+                return None
+
+            preorder_tuple = root.val, preorder(root.left), preorder(root.right)
+            trees[preorder_tuple].append(root)
+            return preorder_tuple
+
+        preorder(root)
+        return [nodes[0] for nodes in trees.values() if len(nodes) > 1]
+
+
+# TODO: write tests
