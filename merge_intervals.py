67444cc3f5acd4e49613377b1058224567e0df2a
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-17 19:48:49 +0530

    Add explanation for merge_intervals

--- merge_intervals.py
+++ merge_intervals.py
@@ -3,22 +3,25 @@ class Solution:
         intervals.sort()
 
         new_intervals: list[list[int]] = []
-
         prev_interval = intervals[0]
         for index in range(1, len(intervals)):
             interval = intervals[index]
 
+            # If the intervals are in ascending order, then they only
+            # overlap if the smaller one's end < larger one's start.
             if interval[0] > prev_interval[1]:
+                # Not overlapping case: create new interval
                 new_intervals.append(prev_interval)
                 prev_interval = interval
             else:
+                # Overlapping case: merge the two intervals
                 prev_interval = [
                     min(prev_interval[0], interval[0]),
                     max(prev_interval[1], interval[1]),
                 ]
 
+        # Add the last interval to the list in the end
         new_intervals.append(prev_interval)
-
         return new_intervals
 
 
