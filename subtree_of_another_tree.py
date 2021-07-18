c1754540959bfe2e15b941a213244a4f0848f3ce
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-19 00:50:33 +0530

    Add subtree_of_another_tree

---
+++ subtree_of_another_tree.py
@@ -0,0 +1,47 @@
+from typing import Callable, Optional
+from testutils.trees import build_tree
+
+
+class TreeNode:
+    def __init__(self, val: int) -> None:
+        self.val = val
+        self.left: Optional['TreeNode'] = None
+        self.right: Optional['TreeNode'] = None
+
+
+def preorder_string(node: Optional[TreeNode]) -> str:
+    """Returns a uniquely identifyable preorder string of the tree"""
+    if not node:
+        return ''
+
+    return f"# {node.val} {preorder_string(node.left)} {preorder_string(node.right)}"
+
+
+class Solution:
+    def isSubtree(self, tree: Optional[TreeNode], subtree: Optional[TreeNode]) -> bool:
+        return preorder_string(subtree) in preorder_string(tree)
+
+
+tests = [
+    (
+        ([3, 4, 5, 1, 2, None, None, None, None, 0], [4, 1, 2],),
+        False,
+    ),
+    (
+        ([3, 4, 5, 1, 2], [4, 1, 2],),
+        True,
+    ),
+]
+
+
+def validator(
+        isSubtree: Callable[[TreeNode, TreeNode], bool],
+        inputs: tuple[list[Optional[int]], list[Optional[int]]],
+        expected: bool,
+) -> None:
+    tree_vals, subtree_vals = inputs
+    tree = build_tree(tree_vals)
+    subtree = build_tree(subtree_vals)
+
+    output = isSubtree(tree, subtree)
+    assert output == expected, (output, expected)
