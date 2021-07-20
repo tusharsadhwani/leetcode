f7128e96b02abe7357d8213bd0dad854545c3ec7
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-20 23:35:30 +0530

    Improve subarray_sum_equals_k

--- subarray_sum_equals_k.py
+++ subarray_sum_equals_k.py
@@ -4,14 +4,24 @@ from collections import defaultdict
 
 class Solution:
     def subarraySum(self, nums: list[int], k: int) -> int:
-        # Sliding window won't work because negative values.
-        subarray_count = 0
-        subarray_sums = list(accumulate(nums, initial=0))
-        subarray_sums_dict: defaultdict[int, int] = defaultdict(int)
+        # NOTE: Sliding window won't work because negative values.
 
-        for _sum in subarray_sums:
-            subarray_count += subarray_sums_dict[_sum-k]
-            subarray_sums_dict[_sum] += 1
+        # Approach:
+        # Every subarray sum can be expressed as:
+        # partial_sum[i] - partial_sum[j]
+        # where partial_sum[x] is sum from 0 to x, and i > j.
+        # So for every partial_sum, we can check if (partial_sum - k)
+        # has occured before, and for each occurence we have an
+        # equivalent subarray that is a valid solution.
+        subarray_count = 0
+        partial_sums = accumulate(nums, initial=0)
+        partial_sum_count: defaultdict[int, int] = defaultdict(int)
+
+        for partial_sum in partial_sums:
+            rest = partial_sum - k
+            subarray_count += partial_sum_count[rest]
+
+            partial_sum_count[partial_sum] += 1
 
         return subarray_count
 
