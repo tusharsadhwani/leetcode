7d60e20eabdde96c27b75dd0ff75af8edda413a6
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-20 22:05:17 +0530

    Add explanation best_time_to_buy_and_sell_stock_ii

--- best_time_to_buy_and_sell_stock_ii.py
+++ best_time_to_buy_and_sell_stock_ii.py
@@ -3,6 +3,14 @@ from typing import Optional
 
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        # Appproach: buy at every valley, sell at every peak
+        # (i.e. when a[i] > a[i-1] and a[i] > a[i+1]).
+        # To do that, we will buy on every moment we are guaranteed a
+        # profit, i.e. whenever the immediately next value is bigger.
+        # Then we sell whenever the immediately next value is smaller.
+        # The only exception is selling at the last value if we have
+        # already bought something (since technically that is the peak
+        # and you can say the next value is 0)
         bought_price: Optional[int] = None
         total_profit = 0
 
